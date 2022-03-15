292e1cb44bd779b24c1e65277b004590aed34401
Firat Kucuk <firat.kucuk@doculayer.com>
2022-03-15 09:48:37 +0100

    Test application created

---
+++ src/main/java/io/firat/liquibasehttpstest/LiquibaseHttpsTestApplication.java
@@ -0,0 +1,13 @@
+package io.firat.liquibasehttpstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseHttpsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseHttpsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/firat/liquibasehttpstest/LiquibaseHttpsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.firat.liquibasehttpstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseHttpsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
